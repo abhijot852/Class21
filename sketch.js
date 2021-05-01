d7c42909a54daa8e0e66af123b38218cf47eb7d3
abhijot852 <82717648+abhijot852@users.noreply.github.com>
2021-05-01 20:42:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+var fixedRect, movingRect;
+var fixedRect1;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  fixedRect1 = createSprite(150,200,30,80);
+  fixedRect1.shapeColor = "green";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+  else if(isTouching(movingRect,fixedRect1)){
+    movingRect.shapeColor = "red";
+    fixedRect1.shapeColor = "red";
+
+
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect1.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+
+
